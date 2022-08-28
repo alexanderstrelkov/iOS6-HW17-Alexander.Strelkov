6cef7a2c9abfb80c7f8198318df0812c12a0506c
Alex <wild6158@gmail.com>
2022-08-28 19:16:17 +0300

    Initial Commit

---
+++ iOS6-HW17-Alexander.Strelkov/iOS6_HW17_Alexander_StrelkovApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS6_HW17_Alexander_StrelkovApp.swift
+//  iOS6-HW17-Alexander.Strelkov
+//
+//  Created by Alexandr Strelkov on 28.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct iOS6_HW17_Alexander_StrelkovApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
